abe6c6aa78a1fe60741e5e0214779e75bbb2f3c1
JackQinzhong <347990469@qq.com>
2019-08-17 14:55:20 +0800

    分布
    分布

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,64 +4,163 @@ const app = getApp()
 
 Page({
 
+  /**
+   * 页面的初始数据
+   */
   data: {
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-  },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    ad_img2: "",
+    jieguo: "",
+    jeguo2: true,
+    indicat: true,
+    vertical: false,
+    autoplay: true,
+    interval: 3000,
+    duration: 500,
+    images: [
+      '/tupian/1.jpg',
+      '/tupian/2.jpg',
+      '/tupian/3.jpg',
+    ],
+
+    teacher: {
+      "Name": "秦钟老师",
+      "address": "湖北监利陶市村"
+    },
+
+    vectfruit: [
+      { fruit: "apple" },
+      { fruit: "banana" },
+      { fruit: "琵琶" },
+      { fruit: "饿胶" }
+
+
+    ]
+    ,
+    userData: [
+      {
+        "data": 101,
+        "title": "2019年春节放假通知道!"
+      },
+      {
+        "data": 102,
+        "title": "年夜饭最新通知"
+      },
+      {
+        "data": 103,
+        "title": "2019年开始上班的通知"
+      }
+    ],
+    nodes: []
+    ,
+    images2: [],
+    //数据不是通过数据库，而是json格式[{"myid":99,"mybiaoti":"2019年春节","myshijian":"02-17"},{}]
+    vectnews: [],
+
+    lastests: []
+
   },
 
-  data: {
-
-  },
-  onLoad: function () {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
     var self = this//重点
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+    wx.request({
+      url: 'http://www.yaoyiwangluo.com/wxshop/100-lunbotupian.html',
+      success: function (res) {
+        self.setData({
+          images: res.data
+
         })
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+    })
+
+    wx.request({
+      url: 'http://www.yaoyiwangluo.com/wxshop/100-lunbotupian-link.html',
+      success: function (res) {
+        self.setData({
+          images2: res.data
+
+        })
+      }
+    })
+    //新闻
+    wx.request({
+      url: 'http://www.yaoyiwangluo.com/wx_news_list.asp',
+      success: function (res) {
+        self.setData({
+          vectnews: res.data
+
+        })
+      }
+    })
+
+    //新闻
+    wx.request({
+      url: 'http://www.yaoyiwangluo.com/wx_CpList_top4.asp',
+      success: function (res) {
+        self.setData({
+          lastests: res.data
+
+        })
+      }
+    })
+
+    var laoshi = {
+
+      "Name": "秦钟",
+      "address": "湖北监利"
     }
+    console.log(laoshi.Name)
+    console.log(laoshi.address)
+  },
 
-
-
-  
-
-
-
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
 
   },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    that.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
 })
